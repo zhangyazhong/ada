8ab0eed4ee6a0e89b76faa36445688cb62de53f8
zhangyazhong <ZhongCentury@gmail.com>
2018-06-10 16:14:20 +0800

    altered pagecounts table to external

--- ada-exp/src/main/java/daslab/restore/DatabaseRestore.java
+++ ada-exp/src/main/java/daslab/restore/DatabaseRestore.java
@@ -5,6 +5,11 @@ import edu.umich.verdict.VerdictSpark2Context;
 import edu.umich.verdict.exceptions.VerdictException;
 import org.apache.spark.sql.SparkSession;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@SuppressWarnings("Duplicates")
 public class DatabaseRestore implements RestoreModule {
     private SparkSession sparkSession;
 
@@ -20,6 +25,7 @@ public class DatabaseRestore implements RestoreModule {
     }
 
     public void restore() {
+        /*
         execute("DROP DATABASE IF EXISTS wiki_ada CASCADE");
         execute("CREATE DATABASE wiki_ada");
         execute("USE wiki_ada");
@@ -28,6 +34,21 @@ public class DatabaseRestore implements RestoreModule {
         for (int day = 1; day <= 7; day++) {
             for (int hour = 0; hour < 24; hour++) {
                 execute(String.format("LOAD DATA LOCAL INPATH '/home/hadoop/wiki/n_pagecounts-201601%02d-%02d0000' INTO TABLE pagecounts", day, hour));
+            }
+        }
+        */
+        call("hadoop fs -rm -r /home/hadoop/spark/wiki_ada_pagecounts");
+        execute("DROP DATABASE IF EXISTS wiki_ada CASCADE");
+        execute("CREATE DATABASE wiki_ada");
+        execute("USE wiki_ada");
+        execute("CREATE EXTERNAL TABLE pagecounts(date_time int, project_name string, page_name string, page_count int, page_size int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/home/hadoop/spark/wiki_ada_pagecounts/'");
+        execute("CREATE TABLE pagecounts_batch(date_time int, project_name string, page_name string, page_count int, page_size int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','");
+        for (int day = 1; day <= 7; day++) {
+            for (int hour = 0; hour < 24; hour++) {
+                String path = String.format("/home/hadoop/wiki/n_pagecounts-201601%02d-%02d0000", day, hour);
+                String command = "hadoop fs -cp " + path + " /home/hadoop/spark/wiki_ada_pagecounts/";
+                AdaLogger.debug(this, "Loading " + path + " into table");
+                call(command);
             }
         }
 
@@ -49,6 +70,25 @@ public class DatabaseRestore implements RestoreModule {
     private void execute(String sql) {
         AdaLogger.debug(this, "About to run: " + sql);
         sparkSession.sql(sql);
+    }
+
+    private void call(String cmd) {
+        AdaLogger.info(this, "About to call: " + cmd);
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new BufferedInputStream(process.getInputStream())));
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+                AdaLogger.debug(this, "System print: " + line);
+            if (process.waitFor() != 0) {
+                if (process.exitValue() == 1) {
+                    AdaLogger.error(this, "Call {" + cmd + "} error!");
+                }
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
